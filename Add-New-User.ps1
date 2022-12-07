7946cf87ca267d4276ae91afccd76c2a6b52d1df
N0ker <115190021+N0ker@users.noreply.github.com>
2022-12-07 14:52:38 +1100

    Update Add-New-User.ps1

--- Add-New-User.ps1
+++ Add-New-User.ps1
@@ -1,1 +1,15 @@
-﻿
+﻿# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Set the username and password for the new user
+$username = "JohnDoe"
+$password = "Pa$$w0rd"
+
+# Set the name and other details for the new user
+$firstName = "John"
+$lastName = "Doe"
+$displayName = "$firstName $lastName"
+$emailAddress = "johndoe@example.com"
+
+# Create the new user account
+New-ADUser -Name $displayName -GivenName $firstName -Surname $lastName -EmailAddress $emailAddress -SamAccountName $username -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -Enabled $true
