5d144ca84643798517f10511c90b7d05a75fb9fb
N0ker <115190021+N0ker@users.noreply.github.com>
2022-12-07 20:41:23 +1100

    Update Add-New-User.ps1

--- Add-New-User.ps1
+++ Add-New-User.ps1
@@ -11,8 +11,16 @@ $lastName = "Doe"
 $displayName = "$firstName $lastName"
 $emailAddress = "johndoe@example.com"
 
+# Set the names of the Active Directory groups to add the user to
+$groups = @("Sales", "Marketing")
+
 # Create the new user account
 New-ADUser -Name $displayName -GivenName $firstName -Surname $lastName -EmailAddress $emailAddress -SamAccountName $username -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -Enabled $true -Path "OU=Users,DC=example,DC=com"
 
+# Add the user to the specified groups
+foreach ($group in $groups) {
+    Add-ADGroupMember -Identity $group -Members $username
+}
+
 # Output a message to confirm the user was created successfully
-Write-Output "User $username was created successfully"
+Write-Output "User $username was created successfully and added to the following groups: $groups"
