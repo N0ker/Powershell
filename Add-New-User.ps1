3fca9ea41508d4475b8bcabc9add243cf61e9420
N0ker <115190021+N0ker@users.noreply.github.com>
2022-12-07 23:04:42 +1100

    Update Add-New-User.ps1
    Added a try catch statement

--- Add-New-User.ps1
+++ Add-New-User.ps1
@@ -14,13 +14,19 @@ $emailAddress = "johndoe@example.com"
 # Set the names of the Active Directory groups to add the user to
 $groups = @("Sales", "Marketing")
 
-# Create the new user account
-New-ADUser -Name $displayName -GivenName $firstName -Surname $lastName -EmailAddress $emailAddress -SamAccountName $username -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -Enabled $true -Path "OU=Users,DC=example,DC=com"
+try {
+    # Create the new user account
+    New-ADUser -Name $displayName -GivenName $firstName -Surname $lastName -EmailAddress $emailAddress -SamAccountName $username -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -Enabled $true -Path "OU=Users,DC=example,DC=com"
 
-# Add the user to the specified groups
-foreach ($group in $groups) {
-    Add-ADGroupMember -Identity $group -Members $username
+    # Add the user to the specified groups
+    foreach ($group in $groups) {
+        Add-ADGroupMember -Identity $group -Members $username
+    }
+
+    # Output a message to confirm the user was created successfully
+    Write-Output "User $username was created successfully and added to the following groups: $groups"
 }
-
-# Output a message to confirm the user was created successfully
-Write-Output "User $username was created successfully and added to the following groups: $groups"
+catch {
+    # Output an error message if the user account or group membership operations fail
+    Write-Error "An error occurred while creating user $username or adding the user to the specified groups. The error message is: $_"
+}
